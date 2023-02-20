8e1277e2eda7d98556ada788fb0abbc4314ad61b
Amin Arria <arria.amin@gmail.com>
2023-02-20 18:28:14 +0100

    Deduplicate code by exporting u64_test_field

--- math/benches/fft.rs
+++ math/benches/fft.rs
@@ -2,67 +2,8 @@ use const_random::const_random;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use lambdaworks_math::fft::fft_cooley_tukey::{fft, inverse_fft};
 use lambdaworks_math::field::element::FieldElement;
-use lambdaworks_math::field::traits::{IsField, IsTwoAdicField};
+use lambdaworks_math::field::test_fields::u64_test_field::U64TestField;
 use rand::Rng;
-
-// Copied from math/src/field/test_fields/u64_test_field.rs
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct U64TestField<const MODULUS: u64>;
-
-impl<const MODULUS: u64> IsField for U64TestField<MODULUS> {
-    type BaseType = u64;
-
-    fn add(a: &u64, b: &u64) -> u64 {
-        ((*a as u128 + *b as u128) % MODULUS as u128) as u64
-    }
-
-    fn sub(a: &u64, b: &u64) -> u64 {
-        (((*a as u128 + MODULUS as u128) - *b as u128) % MODULUS as u128) as u64
-    }
-
-    fn neg(a: &u64) -> u64 {
-        MODULUS - a
-    }
-
-    fn mul(a: &u64, b: &u64) -> u64 {
-        ((*a as u128 * *b as u128) % MODULUS as u128) as u64
-    }
-
-    fn div(a: &u64, b: &u64) -> u64 {
-        Self::mul(a, &Self::inv(b))
-    }
-
-    fn inv(a: &u64) -> u64 {
-        assert_ne!(*a, 0, "Cannot invert zero element");
-        Self::pow(a, MODULUS - 2)
-    }
-
-    fn eq(a: &u64, b: &u64) -> bool {
-        Self::from_u64(*a) == Self::from_u64(*b)
-    }
-
-    fn zero() -> u64 {
-        0
-    }
-
-    fn one() -> u64 {
-        1
-    }
-
-    fn from_u64(x: u64) -> u64 {
-        x % MODULUS
-    }
-
-    fn from_base_type(x: u64) -> u64 {
-        Self::from_u64(x)
-    }
-}
-
-impl<const MODULUS: u64> IsTwoAdicField for U64TestField<MODULUS> {
-    const TWO_ADICITY: u64 = 32;
-    const TWO_ADIC_PRIMITVE_ROOT_OF_UNITY: u64 = 1753635133440165772;
-    const GENERATOR: u64 = 7;
-}
 
 // Mersenne prime numbers
 // https://www.math.utah.edu/~pa/math/mersenne.html
